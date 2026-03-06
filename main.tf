dc1a9b586646ada06ea960d24672a9d2a7d0ebe4
Crystal Mocete <113055503+mocetectec@users.noreply.github.com>
2026-03-06 12:59:11 -0500

    Add files via upload
    Adding main.tf file for this lab.

---
+++ main.tf
@@ -0,0 +1,41 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# 2. Define the Resource Group
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-lab04-tf-[name]"  # CHANGE THIS TO YOUR NAME
+  location = "East US"
+}
+
+# 3. Define the Virtual Network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-terraform"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
+}
+
+#Define a subnet
+resource "azurerm_subnet" "subnet" {
+  name                 = "snet-backend"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+#Define a Network Security Group
+resource "azurerm_network_security_group" "nsg" {
+  name                = "nsg-web"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
